93ed2f37b5b2a83490e870fd6f7cda11901f7d05
Zubeid Hendricks <zubeid.hendricks@gmail.com>
2024-12-27 23:56:59 +0200

    Add Railway deployment configuration

--- src/BlazorPOS.Server/Program.cs
+++ src/BlazorPOS.Server/Program.cs
@@ -1,39 +1,15 @@
-using Microsoft.EntityFrameworkCore;
-using BlazorPOS.Server.Data;
-using BlazorPOS.Server.Services;
-
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddControllers();
-builder.Services.AddRazorPages();
-
-// Add database context
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-// Add services
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
-builder.Services.AddScoped<IOrderService, OrderService>();
-
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseWebAssemblyDebugging();
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+                webBuilder.UseUrls(Environment.GetEnvironmentVariable("HOST") ?? "http://0.0.0.0:8080");
+            });
 }
-else
-{
-    app.UseExceptionHandler("/Error");
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseBlazorFrameworkFiles();
-app.UseStaticFiles();
-app.UseRouting();
-
-app.MapRazorPages();
-app.MapControllers();
-app.MapFallbackToFile("index.html");
-
-app.Run();
